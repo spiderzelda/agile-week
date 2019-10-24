c5ed9474a23c578a512d63655553abaa95c4a9e8
Juan Sebastian Silva <juansebastian.silva@globant.com>
2019-10-23 23:25:14 -0500

    Added new volunteers

--- src/app/home/section-apoio/section-apoio.component.ts
+++ src/app/home/section-apoio/section-apoio.component.ts
@@ -8,15 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class SectionApoioComponent implements OnInit {
     public voluntarios = [
         {
-            name: 'Juanita Ramirez',
+            name: 'Juanita Ramírez',
             picture: '/assets/img/Juanita.jpg',
         },
         {
-            name: 'Sebastian Silva',
+            name: 'Sebastián Silva',
             picture: '/assets/img/sebastian-silva.jpg',
         },
         {
-            name: 'Juan Camilo Rodriguez',
+            name: 'Juan Camilo Rodríguez',
             picture: '/assets/img/juan-camilo-rodriguez.png',
         },
         {
@@ -28,8 +28,56 @@ export class SectionApoioComponent implements OnInit {
             picture: '/assets/img/camilo-cubaque.png',
         },
         {
-            name: 'Andres Gonzalez',
+            name: 'Andrés Gonzalez',
             picture: '/assets/img/andres-gonzalez.jpg',
+        },
+        {
+            name: 'Angélica Rengifo',
+            picture: '/assets/img/angelica-rengifo.png',
+        },
+        {
+            name: 'Claudia Huertas',
+            picture: '/assets/img/claudia-huertas.jpg',
+        },
+        {
+            name: 'Deshy Gasca',
+            picture: '/assets/img/deshy-gasca.jpg',
+        },
+        {
+            name: 'Didier Valencia',
+            picture: '/assets/img/didier-valencia.jpg',
+        },
+        {
+            name: 'Diego Palacios',
+            picture: '/assets/img/diego-palacios.jpg',
+        },
+        {
+            name: 'Fernando Ardila',
+            picture: '/assets/img/fernando-ardila.png',
+        },
+        {
+            name: 'Frederick Gutiérrez',
+            picture: '/assets/img/frederick-gutierrez.jpg',
+        },
+        {
+            name: 'Jose Contreras',
+            picture: '/assets/img/jose-contreras.jpg',
+        },
+        {
+            name: 'Juan Córdoba',
+            picture: '/assets/img/juan-cordoba.jpg',
+        },
+        {
+            name: 'Marcela Vergara',
+            picture: '/assets/img/marcela-vergara.jpeg',
+        },
+        {
+            name: 'Nohora Mondragón',
+            picture: '/assets/img/nohora-mondragon.JPG',
+        },
+        {
+            name: 'Oscar Chavarría',
+            picture: '/assets/img/oscar-chavarria.JPG',
         }
     ];
 
